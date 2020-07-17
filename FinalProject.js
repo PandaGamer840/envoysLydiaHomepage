17942ea508f475d070af9f6c1effc3b53f256dfd
Lydia Osei <lydiaosei@Lydias-MacBook-Pro.local>
2020-07-17 11:37:28 -0400

    Added Final Project

---
+++ FinalProject.js
@@ -0,0 +1,16 @@
+document.getElementById("message1").addEventListener("click", function(){
+    alert("Haha there is no picture!");
+  });
+  document.getElementById("message2").addEventListener("click", function(){
+    alert("Nope still no picture... Maybe the next one?");
+  });
+  document.getElementById("message3").addEventListener("click", function(){
+    alert("Still no... have you given up yet?");
+  });
+  document.getElementById("message4").addEventListener("click", function(){
+    alert("Fineeeee I'll give you a picture, don't say I didn't warn you though...");
+  });
+  document.getElementById("message5").addEventListener("click", function(){
+    document.getElementById("prankTime").src="BlinkingImage2.png"
+  });
+  
